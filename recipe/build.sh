5c68a20ba70b51789ddb515341b53626cf9aef61
Isuru Fernando <isuruf@gmail.com>
2019-07-17 14:46:17 -0500

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=yes \
-    -DBUILD_SHARED_LIBS=yes
+    -DBUILD_SHARED_LIBS=yes \
 ..
 
 make -j${CPU_COUNT}
