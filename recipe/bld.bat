495a8dd5b1ad70a5249de9afcc881954035fb339
Isuru Fernando <isuruf@gmail.com>
2019-07-17 14:56:52 -0500

    Remove -GL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 rm makefile
 copy "%RECIPE_DIR%"\CMakeLists.txt .
 
+REM -GL has issues in cmake
+set "CFLAGS=-MD"
+
 cmake -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE=Release ^
